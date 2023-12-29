3bab8e0c7e1d80f4d67e2fea464e95a901b6cd11
NamanShah2005 <namanhitenshah.com>
2023-12-29 14:39:44 +0530

    fixed engine

--- Data/user.js
+++ Data/user.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
 
 export const connectDB = () => {
-    mongoose.connect(process.env.MONGOCONNECT , {dbName : "backendMain"}).then(() => {
-    console.log("Database connected")
+    mongoose.connect(process.env.MONGOCONNECT , {dbName : "backendMain"}).then((c) => {
+    console.log(`Database connected with ${c.connection.host}`)
 }).catch((e) => {
     console.log(e)
 })
